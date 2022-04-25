84d0e6921d003bd937859a45d54169212b9a1c82
NPCaster <1322880837@qq.com>
2022-04-25 11:59:32 +0800

    TryBranch

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	std::cout << "Hello world!" << std::endl;
+	cout << "Hello world!" << endl;
 	int num = 114514;
+	for(int i = 0; i < num; i++)
+		i += i*7%233;
+	
 	return 0;
 }
